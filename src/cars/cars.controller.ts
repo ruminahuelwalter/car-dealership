1226a0368d6787f721357b88bcf589b92f92c020
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-09-08 18:14:05 -0300

    actualizar un car

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -29,9 +30,9 @@ export class CarsController {
 
     @Patch(':id')
     updateCar( 
-        @Param('id', ParseIntPipe) id:number,
-        @Body() body: any) {
-        return body;
+        @Param('id', ParseUUIDPipe) id: string,
+        @Body() updateCarDto: UpdateCarDto) {
+        return updateCarDto;
     }
     
     @Delete(':id')
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, NotFoundException, Post } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { v4 as uuid } from 'uuid'
-import { CreateCarDto } from './dto/create-car.dto';
+import { CreateCarDto, UpdateCarDto } from './dto';
+
 
 @Injectable()
 export class CarsService {
@@ -49,5 +50,9 @@ export class CarsService {
         this.cars.push( car );
        
         return car;
+    }
+
+    update( id:string, updateCarDto:UpdateCarDto ) {
+        
     }
 }
