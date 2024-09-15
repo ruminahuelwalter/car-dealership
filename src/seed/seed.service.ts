4ecb1fbfbd4ebb036859c50f8318ce1b0d44cf43
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-09-15 20:11:34 -0300

    Inyectar servicios en otros servicios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from 'src/cars/cars.service';
+import { CARS_SEED } from './data/cars.seed';
+import { BrandsService } from 'src/brands/brands.service';
+import { BRANDS_SEED } from './data/brands.seed';
 
 @Injectable()
 export class SeedService {
 
+  constructor(
+    private readonly carsService: CarsService, 
+    private readonly brandsService: BrandsService 
+  ){}
 
   populateDB() {
+
+    this.carsService.fillCarsWithSeedData( CARS_SEED );
+    this.brandsService.fillCarsWithSeedData( BRANDS_SEED );
 
   return 'Seed executed '
  }
